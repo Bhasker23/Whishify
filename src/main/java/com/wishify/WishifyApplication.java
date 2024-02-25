78a7af8fcb6f5ece214dc3ad8cdf329a4fbe9b3e
Bhasker23 <shrimalibhaskar2398@gmail.com>
2024-02-25 14:15:11 +0530

    project structure with DB Intrigation done

---
+++ src/main/java/com/wishify/WishifyApplication.java
@@ -0,0 +1,13 @@
+package com.wishify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishifyApplication.class, args);
+    }
+
+}
